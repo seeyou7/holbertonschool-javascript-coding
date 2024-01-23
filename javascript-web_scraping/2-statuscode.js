f3c041880b61ffec8352892660297692ce086644
seeyou7 <5764@holbertonstudents.com>
2024-01-23 22:38:18 +0100

    remove comment

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-// Get the URL from the command line arguments
+
 const url = process.argv[2];
 
-// using GET methode to request to the specified URL
 request.get(url).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
