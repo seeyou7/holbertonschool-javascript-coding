221ab486fe66246cdf70098b442942318b35fdac
seeyou7 <5764@holbertonstudents.com>
2024-01-24 10:38:59 +0100

    push test

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 request(process.argv[2], (error, response) => {
   if (error) console.log(error);
   console.log(`code: ${response.statusCode}`);
