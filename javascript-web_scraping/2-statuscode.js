2c987b151d6eda22752f9a89ec5f3d6c15a9dd81
seeyou7 <5764@holbertonstudents.com>
2024-03-17 18:29:52 +0100

    Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-
+const url = process.argv[2];
 const request = require('request');
 
-request(process.argv[2], (error, response) => {
-  if (error) console.log(error);
-  console.log(`code: ${response.statusCode}`);
-});
+request
+  .get(url)
+  .on('response', function (response) {
+    console.log(`code: ${response.statusCode}`);
+  });
